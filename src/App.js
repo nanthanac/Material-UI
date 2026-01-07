0413711ad7c9eabc2b7088369d5085ae9f64b664
nanthanac <cnanthana766@gmail.com>
2026-01-07 12:01:40 +0530

    Added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import ControlledAccordions from './components/Accord';
+import MenuAppBar from './components/ButtonAppBar';
+
+function App() {
+  return (
+    <div className="App">
+      <MenuAppBar/>
+      <ControlledAccordions/>
+    </div>
+  );
+}
+
+export default App;
